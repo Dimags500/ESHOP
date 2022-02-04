0315aaf967923c4c2a086290ab183ba8f040a7ea
Dimags500 <73039506+Dimags500@users.noreply.github.com>
2022-02-04 11:07:03 +0200

    roution added

--- Client/src/app/shop/shop.component.ts
+++ Client/src/app/shop/shop.component.ts
@@ -63,11 +63,13 @@ sortOptions = [
 
     onBrandSelected(brandId : number){
       this.shopParams.brandId = brandId ;
+      this.shopParams.pageNumber =1;
       this.getProducts();
     }
 
     onTypeSelected(typeId : number){
       this.shopParams.typeId = typeId ;
+      this.shopParams.pageNumber =1;
       this.getProducts();
     }
 
@@ -77,13 +79,17 @@ sortOptions = [
     }
 
     onPageChanged(event : any){
-      this.shopParams.pageNumber = event ; 
-      this.getProducts() ;
+      
+      if(this.shopParams.pageNumber !== event){
+        this.shopParams.pageNumber = event ;
+        this.getProducts();
+      }
     }
 
 
     onSearch() {
       this.shopParams.search = this.searchTerm.nativeElement.value ;
+      this.shopParams.pageNumber =1;
       this.getProducts();
     }
     onReset(){
